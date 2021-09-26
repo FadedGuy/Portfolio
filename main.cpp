a2ee2cc08012e0b33bc6566070ee92a03d5466a0
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2021-09-26 22:15:23 +0200

    llenar matriz, imprimir palabras y matriz funciones

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
-#include <cstdio>
 #include <string>
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <algorithm>
+#include <time.h>
+
+#define MAX_SIZE_MAT 35
+
+/********************
+ * Tiene que haber minimo 2 palabras en la sopa de letras
+ * El tam minimo de la palabra mas larga es de 3 y el maximo
+ * de MAX_SIZE_MAT
+ ********************/
 
 int checkFile(std::string filename)
 {
@@ -15,16 +24,13 @@ int checkFile(std::string filename)
     return 0;
 }
 
-std::vector<std::string> getWordList(std::string filename)
+void getWordList(std::string filename, std::vector<std::string> &wordList, int &maxLength)
 {
-    std::vector<std::string> wordList;
     std::ifstream file(filename);
-    int maxLength = 0;
 
     if(!file.is_open())
     {
         std::cerr<<"Unable to open file: " << filename << "\n";
-        return wordList;
     }
     else
     {
@@ -35,23 +41,49 @@ std::vector<std::string> getWordList(std::string filename)
             {
                 line += toupper(c);
             } 
-            else if(c == '\n' || c == ',' || c == ' ')
+            else if(c == '\n' || c == ',')
             {
-                if(line.size() > maxLength)
+                if(line.size() > maxLength) maxLength = line.size();
+                if(line.size() > 0)
                 {
-                    maxLength = line.size();
+                    wordList.push_back(line);
+                    line = "";
                 }
-                wordList.push_back(line);
-                line = "";
-            }
-            else
-            {
-                //Ignorar caracter no valido
             }
         }
     }
-    wordList.push_back(std::to_string(maxLength));
-    return wordList;
+}
+
+void fillMatrixRandom(char mat[MAX_SIZE_MAT][MAX_SIZE_MAT], int maxL)
+{
+    srand(time(NULL));
+    for(int i = 0; i < maxL; i++)
+    {   
+        for(int j = 0; j < maxL; j++)
+        {
+            mat[i][j] = 'A' + std::rand()%26;
+        }
+    }
+}
+
+void printMatrix(char mat[MAX_SIZE_MAT][MAX_SIZE_MAT], int maxL)
+{
+    for(int i = 0; i < maxL; i++)
+    {   
+        for(int j = 0; j < maxL; j++)
+        {
+            std::cout<<mat[i][j]<<" ";
+        }
+        std::cout<<"\n";
+    }
+}
+
+void printAllWords(std::vector<std::string> &wordList)
+{
+    for(int i = 0; i < wordList.size(); i++)
+    {
+        std::cout<<i<<". "<<wordList[i]<<"\n";
+    }
 }
 
 int main(int argc, char *argv[])
@@ -65,11 +97,21 @@ int main(int argc, char *argv[])
     if(checkFile(argv[1]) == 0)
     {
         std::vector<std::string> wordList;
-        wordList = getWordList(argv[1]);   
-        for(int i = 0; i <= wordList.size()-1; i ++){
-            std::cout<<wordList[i]<<"\n";
-        }     
-        std::cout<<wordList[wordList.size()];
+        int maxSizeWord = 0;
+        getWordList(argv[1], wordList, maxSizeWord);
+        if(wordList.size() > 1 && maxSizeWord >= 2 && maxSizeWord <= MAX_SIZE_MAT)
+        {
+            // std::sort(wordList.begin(), wordList.end()); //No se si ordenarlas ayude realmente
+            char matrix[MAX_SIZE_MAT][MAX_SIZE_MAT];
+            fillMatrixRandom(matrix, maxSizeWord);
+            printMatrix(matrix, maxSizeWord);
+            printAllWords(wordList);
+
+        }
+        else
+        {
+            std::cout<<"Error while parsing file: "<<argv[1]<<"\n";
+        }
     }
     return 0;
 }
