23ac6edc7e396adaf3b555205a341210c38cf8ff
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2021-09-27 20:07:08 +0200

    Si

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 #include <iostream>
 #include <algorithm>
 #include <time.h>
+#include <map>
 
 #define MAX_SIZE_MAT 35
+#define MAX_WORDS 10
 
 /********************
  * Tiene que haber minimo 2 palabras en la sopa de letras
- * El tam minimo de la palabra mas larga es de 3 y el maximo
- * de MAX_SIZE_MAT
+ * El tam minimo de cada palabra es de 3 y el maximo de MAX_SIZE_MAT
  ********************/
 
 int checkFile(std::string filename)
@@ -24,7 +25,7 @@ int checkFile(std::string filename)
     return 0;
 }
 
-void getWordList(std::string filename, std::vector<std::string> &wordList, int &maxLength)
+void getWordList(std::string filename, std::vector<std::string> &wordList, int &maxLength, std::map<char, int> wordListMap[], int &cnt)
 {
     std::ifstream file(filename);
 
@@ -36,10 +37,11 @@ void getWordList(std::string filename, std::vector<std::string> &wordList, int &
     {
         std::string line;
         char c;
-        while(file.get(c)){
+        while(file.get(c) && wordList.size() < MAX_WORDS){
             if(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
             {
                 line += toupper(c);
+                wordListMap[cnt][toupper(c) - 'A']++;
             } 
             else if(c == '\n' || c == ',')
             {
@@ -47,6 +49,7 @@ void getWordList(std::string filename, std::vector<std::string> &wordList, int &
                 if(line.size() > 0)
                 {
                     wordList.push_back(line);
+                    cnt++;
                     line = "";
                 }
             }
@@ -61,7 +64,8 @@ void fillMatrixRandom(char mat[MAX_SIZE_MAT][MAX_SIZE_MAT], int maxL)
     {   
         for(int j = 0; j < maxL; j++)
         {
-            mat[i][j] = 'A' + std::rand()%26;
+            //mat[i][j] = 'A' + std::rand()%26;
+            mat[i][j] = '0';
         }
     }
 }
@@ -86,28 +90,30 @@ void printAllWords(std::vector<std::string> &wordList)
     }
 }
 
-
-
-void insertWordMatrix(char mat[MAX_SIZE_MAT][MAX_SIZE_MAT], std::string word, int maxSize)
+void insertWordMatrix(char mat[MAX_SIZE_MAT][MAX_SIZE_MAT], std::vector<std::string> &words, int maxSize, int iPos, std::map<char, int> wordListMap[], int sizeWordListMapped)
 {
+    /*********************
+     * wordListMap podria usarse para ver si una palabra cruza en alguna letra con las palabras anteriores para saber si es que un cruce es favorable
+     * Espero que esto deberia de solucionar tambien las colisiones entre las palabras que actualmente hay
+     *********************/
     int randLine = rand() % maxSize; //Elige la linea en la que ira la palabra
     int verticalHorizontal = rand() % 2; //Elige si va en vertical o horizontal (a futuro tambien diagonal)
-    int init = 0; 
-    if(maxSize - word.size() > 0){init = rand()% (maxSize - word.size());} //Elige en que posicion inicia la palabra dentro de los limites y de su tamano. Default 0
+    int init = 0;
+    if(maxSize - words[iPos].size() > 0){init = rand()% (maxSize - words[iPos].size());} //Elige en que posicion inicia la palabra dentro de los limites y de su tamano. Default 0
     std::cout<<"Line: " << randLine << " Vertical: " << verticalHorizontal << " Init: " << init << "\n";
     if(verticalHorizontal == 0) //Vertical line
     {
-        for(int j = 0; j < word.size(); j++)
+        for(int j = 0; j < words[iPos].size(); j++)
         {
-            mat[init][randLine] = word[j];
+            mat[init][randLine] = words[iPos][j];
             init++;
         }
         }
     else //Horizontal line
     {
-        for(int j = 0; j < word.size(); j++)
+        for(int j = 0; j < words[iPos].size(); j++)
         {
-            mat[randLine][init] = word[j];
+            mat[randLine][init] = words[iPos][j];
             init++;
         }
     }
@@ -125,22 +131,31 @@ int main(int argc, char *argv[])
     if(checkFile(argv[1]) == 0)
     {
         std::vector<std::string> wordList;
+        std::map<char, int> wordListMapped[MAX_WORDS];
         int maxSizeWord = 0;
-        getWordList(argv[1], wordList, maxSizeWord);
+        int sizeWordListMapped = 0;
+        getWordList(argv[1], wordList, maxSizeWord, wordListMapped, sizeWordListMapped);
         if(wordList.size() > 1 && maxSizeWord >= 2 && maxSizeWord <= MAX_SIZE_MAT)
         {
+            // for(int i = 0; i < sizeWordListMapped; i++)
+            // {
+            //     for(int j = 0; j < wordListMapped[i].size(); j++)
+            //     {
+            //         std::cout<<wordListMapped[i][j]<<" ";
+            //     }
+            //     std::cout<<"\t:"<<wordList[i];
+            //     std::cout<<"\n";
+            // }
             char matrix[MAX_SIZE_MAT][MAX_SIZE_MAT];
             fillMatrixRandom(matrix, maxSizeWord);
             printMatrix(matrix, maxSizeWord);
-            // std::sort(wordList.begin(), wordList.end()); //No se si ordenarlas ayude realmente
             printAllWords(wordList);
 
             //mat[i][j] i va para abajo, j para el lado
-            
             srand(time(NULL));
             for(int i = 0; i < wordList.size(); i++)
             {
-                insertWordMatrix(matrix, wordList[i], maxSizeWord);
+                insertWordMatrix(matrix, wordList, maxSizeWord, i, wordListMapped, sizeWordListMapped);
             }
             printMatrix(matrix, maxSizeWord);
         }
