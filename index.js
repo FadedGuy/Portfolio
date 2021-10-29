305a953c791a0860bbbed0d63c8d907c74094282
Kevin Damian Aceves Siordia <kevin.aceves@hotmail.com>
2021-10-29 14:20:47 +0200

    serveFiles

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 3000;
 const file = new(static.Server)(__dirname);
 
 const server = http.createServer((req, res) => {
-   res.writeHead(200, { 'Content-Type': 'text/html' });
+    file.serveFile('index.xhtml', 200, {'Content-Type' : 'text/html'}, req, res);
+   /*res.writeHead(200, { 'Content-Type': 'text/html' });
 
    fs.readFile('index.xhtml', (error, data) => {
        if(error){
@@ -18,7 +19,7 @@ const server = http.createServer((req, res) => {
             res.write(data);
         }
         res.end();
-    });
+    });*/
 });
 
 server.listen(port, (error) => {
