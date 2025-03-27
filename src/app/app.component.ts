18076b0643c6253af7c6ebc2dc20e82f0e44a051
Kevin Aceves <kevin.aceves@hotmail.com>
2025-03-27 14:38:04 -0600

    Enter environment prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio';
+  isProd = environment.production;
 }
